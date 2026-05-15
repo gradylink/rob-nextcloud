d2e312440799e2abfe9eb38001b04779d433669c
Grady Link <github@grady.link>
2026-05-14 23:33:23 -0700

    feat: init

---
+++ env.d.ts
@@ -0,0 +1,10 @@
+declare module "bun" {
+  interface Env {
+    GROQ_API_KEY: string;
+
+    NEXTCLOUD_URL: string;
+    NEXTCLOUD_USERNAME: string;
+    NEXTCLOUD_PASSWORD: string;
+    NEXTCLOUD_CONVERSATION_TOKEN: string;
+  }
+}
