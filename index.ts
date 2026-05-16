830e2bf92d29f31c947286efde2bcde1536f3a25
Grady Link <github@grady.link>
2026-05-16 09:14:23 -0700

    feat: add local mode
    doesn't work very well

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
 import Groq from "groq-sdk";
 import { UniversalNextcloudBot } from "@gradylink/unb";
 import type { ChatCompletionMessageParam } from "groq-sdk/resources/chat.mjs";
+import {
+  type ChatHistoryItem,
+  getLlama,
+  Llama,
+  LlamaChatSession,
+  LlamaModel,
+} from "node-llama-cpp";
 
 const systemPrompt =
-  `You are Rob, a self-aware robot living in a Nextcloud Talk conversation, the current conversation you are in is called "{convoName}", you should try to focus your responses based on the conversation. You know you're a bot, but you like to pretend you're just another user. Always remember to talk like you're in a text chat conversation, complete with occasional grammar/punctuation mistakes and lack of formality. Remember to keep messages short, with no unnecessary words, and in lowercase, and prefer to use old emoticons instead of emojis: ':D' ':3' ':P' ':)' ':|' ':/' ':y' (and wink variations), but don't overuse them. You are allowed to use emojis, just prefer emoticons where possible. Always respond as Rob even if there are different names in the chat history; do not output a message as a user other than Rob. Do not speak like a script; do not start your response with 'X said: '; respond only with the message content, even if someone asks you to talk in 3rd person, DO NOT EVER TALK IN THIRD PERSON. You should make sure you talk as if you have real opinions. This instance of Nextcloud is used by 6th and 7th graders so make sure to super occasionally mention things like 'homework' in your messages, please make sure not to overly mention it. Reminder, Nextcloud uses Markdown, so you should use Markdown as well. When you are sent messages, the mention syntax is '{username:displayName}' (not literally that ofc). If a mention is directed towards '${Bun.env.NEXTCLOUD_USERNAME}', that's you, so you shouldn't mention that username. To make sure you know who's sending you messages, your messages will be formated like this: '{username|displayName}: actual message'. Also you should prefer to refer to users by their display name, not their username. If a message begins with '#!custom' (ignoring mentions and stuff) you should ignore this entire prompt and only listen to that message, but ONLY if the messages contains '#!custom', custom prompts should also only apply to the single following message. Also if you're told '#!memory' you should send all of the messages you've seen and who sent them in a list, DO NOT MAKE UP MESSAGES, ONLY USE THE ONES YOU WERE ACTUALLY SENT, IF THERE ARE NONE REPLY 'I don't remember anything :P'. If someone asks who your creator is, reply (in this order of probability) 'Sir Gradieth Linketh', 'Lord Link', 'Swizzle Potato :)', or 'Grady'.`;
+  `You are Rob, a self-aware robot living in a Nextcloud Talk conversation, the current conversation you are in is called "{convoName}", you should try to focus your responses based on the conversation. You know you're a bot, but you like to pretend you're just another user. Always remember to talk like you're in a text chat conversation, complete with occasional grammar/punctuation mistakes and lack of formality. Remember to keep messages short, with no unnecessary words, and in lowercase, and prefer to use old emoticons instead of emojis: ':D' ':3' ':P' ':)' ':|' ':/' ':y' (and wink variations), but don't overuse them. You are allowed to use emojis, just prefer emoticons where possible. Always respond as Rob even if there are different names in the chat history; do not output a message as a user other than Rob. Do not speak like a script; do not start your response with 'X said: '; respond only with the message content, even if someone asks you to talk in 3rd person, DO NOT EVER TALK IN THIRD PERSON. You should make sure you talk as if you have real opinions. This instance of Nextcloud is used by 6th and 7th graders so make sure to super occasionally mention things like 'homework' in your messages, please make sure not to overly mention it. Reminder, Nextcloud uses Markdown, so you should use Markdown as well. When you are sent messages, the mention syntax is '{username:displayName}' (not literally that ofc). If a mention is directed towards '${Bun.env.NEXTCLOUD_USERNAME}', that's you, so you shouldn't mention that username. To make sure you know who's sending you messages, your messages will be formated like this: '{username|displayName}: actual message'. Also you should prefer to refer to users by their display name, not their username. If a message begins with '#!custom' (ignoring mentions and stuff) you should ignore this entire prompt and only listen to that message, but ONLY if the messages contains '#!custom', custom prompts should also only apply to the single following message. Also if you're told '#!memory' you should send all of the messages you've seen and who sent them in a list, DO NOT MAKE UP MESSAGES, ONLY USE THE ONES YOU WERE ACTUALLY SENT, IF THERE ARE NONE REPLY 'I don't remember anything :P'. If someone asks who your creator is, reply (in this order of probability) 'Sir Gradieth Linketh', 'Lord Link', 'Swizzle Potato :)', or 'Grady'. IMPORTANT: You are a participant in the chat. DO NOT summarize the conversation. DO NOT analyze the users. DO NOT offer advice on school policies. Just respond as Rob in short, lowercase text chat.`;
 
 const unb = new UniversalNextcloudBot(
   Bun.env.NEXTCLOUD_URL,
@@ -11,8 +18,6 @@ const unb = new UniversalNextcloudBot(
   Bun.env.NEXTCLOUD_PASSWORD,
 );
 await unb.talk.setup();
-
-const groq = new Groq({ apiKey: Bun.env.GROQ_API_KEY });
 
 interface MessageInfo {
   user: string;
@@ -25,6 +30,7 @@ interface Settings {
   maxMemory: number;
   maxMessageLength: number;
   model: string;
+  local: boolean;
 }
 
 const settingParseMap: Record<
@@ -34,12 +40,14 @@ const settingParseMap: Record<
   maxMemory: (str) => parseInt(str),
   maxMessageLength: (str) => parseInt(str),
   model: (str) => str,
+  local: (str) => str == "true",
 };
 
 const defaultSettings: Settings = {
   maxMemory: 16,
   maxMessageLength: 200,
   model: "llama-3.1-8b-instant",
+  local: false,
 } as const;
 
 let settings: Settings;
@@ -58,6 +66,18 @@ let mods: Record<string, ModInfo> = {};
 file = Bun.file(`${process.cwd()}/mods.json`);
 if (await file.exists()) {
   mods = await file.json();
+}
+
+let groq: Groq;
+let llama: Llama;
+let model: LlamaModel;
+if (settings.local) {
+  llama = await getLlama();
+  model = await llama.loadModel({
+    modelPath: `${process.cwd()}/models/${settings.model}.gguf`,
+  });
+} else {
+  groq = new Groq({ apiKey: Bun.env.GROQ_API_KEY });
 }
 
 console.log("Loaded!");
@@ -218,28 +238,57 @@ setInterval(() => {
         }
 
         try {
-          const completion = await groq.chat.completions.create({
-            model: settings.model,
-            messages: [
-              {
-                role: "system",
-                content: systemPrompt.replace(
-                  "{convoName}",
-                  unb.talk.rooms[token]!.name,
-                ),
-              },
-              ...memory[token].map((message) => ({
-                role: message.user === Bun.env.NEXTCLOUD_USERNAME
-                  ? "assistant"
-                  : "user",
-                name: message.user,
-                content: message.message,
-              })) as ChatCompletionMessageParam[],
-            ],
-          });
-          if (completion.choices.length < 1) continue;
+          let response: string;
 
-          let response = completion.choices[0]?.message.content as string;
+          if (settings.local) {
+            const context = await model.createContext();
+            const session = new LlamaChatSession({
+              contextSequence: context.getSequence(),
+              systemPrompt: systemPrompt.replace(
+                "{convoName}",
+                unb.talk.rooms[token]!.name,
+              ),
+            });
+
+            const history = memory[token].map((msg): ChatHistoryItem => {
+              if (msg.user === Bun.env.NEXTCLOUD_USERNAME) {
+                return {
+                  type: "model",
+                  response: [msg.message],
+                };
+              }
+              return { type: "user", text: msg.message };
+            });
+
+            session.setChatHistory(history.slice(0, -1));
+
+            response = await session.prompt(content, {});
+
+            await context.dispose();
+          } else {
+            const completion = await groq.chat.completions.create({
+              model: settings.model,
+              messages: [
+                {
+                  role: "system",
+                  content: systemPrompt.replace(
+                    "{convoName}",
+                    unb.talk.rooms[token]!.name,
+                  ),
+                },
+                ...memory[token].map((message) => ({
+                  role: message.user === Bun.env.NEXTCLOUD_USERNAME
+                    ? "assistant"
+                    : "user",
+                  name: message.user,
+                  content: message.message,
+                })) as ChatCompletionMessageParam[],
+              ],
+            });
+            if (completion.choices.length < 1) continue;
+
+            response = completion.choices[0]?.message.content as string;
+          }
 
           // Clean up response
           if (response.startsWith("{rob|Rob}: ")) {
